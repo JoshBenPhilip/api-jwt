0a59b508e8a2dcb5bb98acd03be80ddf3a52f91c
JoshBenPhilip <joshben4c@gmail.com>
2022-06-07 15:27:16 -0400

    working

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,27 @@ import cors from "cors";
 import jwt from "jsonwebtoken";
 import mySecretKey from "./secret.js";
 
+// vitoria ghi789
+//damian def456
+//todd abc123
+
 const users = [
-  { id: 1, email: "todd@bocacode.com", password: "abc123" },
-  { id: 2, email: "damian@bocacode.com", password: "def456" },
-  { id: 3, email: "vitoria@bocacode.com", password: "ghi789" },
+  // fake database
+  {
+    id: 1,
+    email: "todd@bocacode.com",
+    password: "$2b$10$r7QjINHBd6tv/M6UgNs5muAY5vc2NS0woClsP9b2QPwEK49x7c/yG",
+  },
+  {
+    id: 2,
+    email: "damian@bocacode.com",
+    password: "$2b$10$r7QjINHBd6tv/M6UgNs5muA67xV0yZTIjuLZyv1O9xGuFXht.ukQS",
+  },
+  {
+    id: 3,
+    email: "vitoria@bocacode.com",
+    password: "$2b$10$r7QjINHBd6tv/M6UgNs5mu0XseAuWTpEyD9SSwbytE1EGJAJ5oXCq",
+  },
 ];
 
 const app = express();
@@ -23,27 +40,27 @@ app.post("/login", (req, res) => {
     (user) => user.email === email && user.password === password
   );
   if (!user) {
-    res.status(401).send("Invalid email or password");
+    res.status(401).send({ error: "Invalid email or password" });
     return;
   }
   user.password = undefined; // remove password from the user object
   // now we want to sign / create a token...
   const token = jwt.sign(user, mySecretKey, { expiresIn: "1h" });
-  res.send(token);
+  res.send({ token });
 });
 app.get("/public", (req, res) => {
-  res.send("Welcome!"); // anyone can see this..
+  res.send({ message: "Welcome!" }); // anyone can see this..
 });
 app.get("/private", (req, res) => {
   // let's require a valid token to see this
   const token = req.headers.authorization || "";
   if (!token) {
-    res.status(401).send("You must be logged in to see this");
+    res.status(401).send({ error: "You must be logged in to see this" });
     return;
   }
   jwt.verify(token, mySecretKey, (err, decoded) => {
     if (err) {
-      res.status(401).send("You must use a valid token to see this");
+      res.status(401).send({ error: "You must use a valid token to see this" });
       return;
     }
     // here we know that the token is valid...
